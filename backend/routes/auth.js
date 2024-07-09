e77419cad4fbdd06435ae444638ba114c6ec87df
AnuragRajput1202 <anuragrajput06@gmail.com>
2024-07-09 10:55:55 +0530

    updated upload directory

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -7,7 +7,7 @@ const { validationResult, body } = require('express-validator')
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, './uploads/')
+        cb(null, './backend/uploads/')
     },
     filename: function (req, file, cb) {
         const uniquePrefix = Date.now()
